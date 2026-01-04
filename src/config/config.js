61751fbd19c1e7cf69d697bda74c8d5b19c4783e
Yogesh GR <yogeshgr@Yogeshs-MacBook-Air.local>
2026-01-04 10:23:10 +0530

    fix(vercel): revert to proxy and simplify rewrites

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-  serverUrl: import.meta.env.VITE_APP_SERVERURL || "https://eren-world.onrender.com/api/v1",
+  serverUrl: import.meta.env.VITE_APP_SERVERURL || "/api",
   localUrl: import.meta.env.VITE_APP_LOCALURL || "http://localhost:3030/api/v1",
   proxyUrl: import.meta.env.VITE_APP_PROXYURL || "/api",
 };
